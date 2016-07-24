c20e59a8f5d9fbce8afeb829ae1f7f10d101a13e
Anaggrama <lucho.blg@hotmail.com>
2016-07-24 14:51:27 -0300

    Uploaded the PathFinding program.

---
+++ PathFinding/Main.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Pantalla = new System.Windows.Forms.PictureBox();
+            this.cmdSimplePath = new System.Windows.Forms.Button();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.cmdClearPath = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTiempo = new System.Windows.Forms.TextBox();
+            this.txtIteraciones = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainMenu = new System.Windows.Forms.MainMenu(this.components);
+            this.mnuFile = new System.Windows.Forms.MenuItem();
+            this.mnuOptions = new System.Windows.Forms.MenuItem();
+            this.mnuDTempPath = new System.Windows.Forms.MenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Pantalla)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pantalla
+            // 
+            this.Pantalla.Location = new System.Drawing.Point(-2, 1);
+            this.Pantalla.Name = "Pantalla";
+            this.Pantalla.Size = new System.Drawing.Size(401, 301);
+            this.Pantalla.TabIndex = 0;
+            this.Pantalla.TabStop = false;
+            this.Pantalla.Click += new System.EventHandler(this.Pantalla_Click);
+            this.Pantalla.Paint += new System.Windows.Forms.PaintEventHandler(this.Pantalla_Paint);
+            this.Pantalla.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pantalla_MouseMove);
+            // 
+            // cmdSimplePath
+            // 
+            this.cmdSimplePath.Location = new System.Drawing.Point(407, 4);
+            this.cmdSimplePath.Name = "cmdSimplePath";
+            this.cmdSimplePath.Size = new System.Drawing.Size(143, 28);
+            this.cmdSimplePath.TabIndex = 1;
+            this.cmdSimplePath.Text = "Simple PathFinding";
+            this.cmdSimplePath.UseVisualStyleBackColor = true;
+            this.cmdSimplePath.Click += new System.EventHandler(this.cmdSimplePath_Click);
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Location = new System.Drawing.Point(407, 184);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(136, 23);
+            this.cmdClear.TabIndex = 2;
+            this.cmdClear.Text = "Clear Screen";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // cmdClearPath
+            // 
+            this.cmdClearPath.Location = new System.Drawing.Point(407, 204);
+            this.cmdClearPath.Name = "cmdClearPath";
+            this.cmdClearPath.Size = new System.Drawing.Size(135, 21);
+            this.cmdClearPath.TabIndex = 3;
+            this.cmdClearPath.Text = "Clear Path";
+            this.cmdClearPath.UseVisualStyleBackColor = true;
+            this.cmdClearPath.Click += new System.EventHandler(this.cmdClearPath_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTiempo);
+            this.groupBox1.Controls.Add(this.txtIteraciones);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(407, 232);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 69);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            // 
+            // txtTiempo
+            // 
+            this.txtTiempo.Location = new System.Drawing.Point(74, 41);
+            this.txtTiempo.Name = "txtTiempo";
+            this.txtTiempo.Size = new System.Drawing.Size(56, 20);
+            this.txtTiempo.TabIndex = 3;
+            // 
+            // txtIteraciones
+            // 
+            this.txtIteraciones.Location = new System.Drawing.Point(74, 12);
+            this.txtIteraciones.Name = "txtIteraciones";
+            this.txtIteraciones.Size = new System.Drawing.Size(56, 20);
+            this.txtIteraciones.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Iterations:";
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.mnuFile,
+            this.mnuOptions});
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.Index = 0;
+            this.mnuFile.Text = "File";
+            // 
+            // mnuOptions
+            // 
+            this.mnuOptions.Index = 1;
+            this.mnuOptions.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.mnuDTempPath});
+            this.mnuOptions.Text = "Options";
+            // 
+            // mnuDTempPath
+            // 
+            this.mnuDTempPath.Index = 0;
+            this.mnuDTempPath.Text = "Draw Temporary Path";
+            this.mnuDTempPath.Click += new System.EventHandler(this.mnuDTempPath_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 310);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmdClearPath);
+            this.Controls.Add(this.cmdClear);
+            this.Controls.Add(this.cmdSimplePath);
+            this.Controls.Add(this.Pantalla);
+            this.Menu = this.MainMenu;
+            this.Name = "Main";
+            this.Text = "PathFinding";
+            ((System.ComponentModel.ISupportInitialize)(this.Pantalla)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pantalla;
+        private System.Windows.Forms.Button cmdSimplePath;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.Button cmdClearPath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTiempo;
+        private System.Windows.Forms.TextBox txtIteraciones;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MainMenu MainMenu;
+        private System.Windows.Forms.MenuItem mnuFile;
+        private System.Windows.Forms.MenuItem mnuOptions;
+        private System.Windows.Forms.MenuItem mnuDTempPath;
+    }
+}
+
---
+++ PathFinding/Main.cs
@@ -0,0 +1,499 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Main : Form
+    {
+        public int MapSizeX = 40;
+        public int MapSizeY = 30;
+
+        sMapData[,] MapData = new sMapData[40, 30];
+        sPos StartPos = new sPos(1, 4);
+        sPos EndPos = new sPos(25, 27);
+
+        sOptions Options = new sOptions();
+
+        public int LastMouseX;
+        public int LastMouseY;
+
+        public Main()
+        {
+            InitializeComponent();
+            Pantalla.Paint += new PaintEventHandler(Pantalla_Paint);
+            LoadMap();
+
+            Options.ShowTempPaths = false;
+            mnuDTempPath.Checked = Options.ShowTempPaths;
+        }
+
+        private void Pantalla_Paint(object sender, PaintEventArgs e)
+        {
+            int x;
+            int y;
+
+            Graphics grp = e.Graphics;
+
+            for (x = 0; x <= 39; x++)
+            {
+                for (y = 0; y <= 29; y++)
+                {
+                    if (MapData[x, y].Bloq == 1)
+                    {
+                        SolidBrush brush = new SolidBrush(Color.Red);
+                        grp.FillRectangle(brush, x * 10, y * 10, 10, 10);
+                    }
+                    if (MapData[x, y].Type == 1)
+                    {
+                        SolidBrush brush = new SolidBrush(Color.Blue);
+                        grp.FillRectangle(brush, x * 10, y * 10, 10, 10);
+                    }
+                    else if (MapData[x, y].Type == 2)
+                    {
+                        SolidBrush brush = new SolidBrush(Color.Green);
+                        grp.FillRectangle(brush, x * 10, y * 10, 10, 10);
+                    }
+                    else if (MapData[x, y].tmpPath == 1)
+                    {
+                        SolidBrush brush = new SolidBrush(Color.Yellow);
+                        grp.FillRectangle(brush, x * 10, y * 10, 10, 10);
+                    }
+                    if (MapData[x, y].Path == 1)
+                    {
+                        SolidBrush brush = new SolidBrush(Color.Orange);
+                        grp.FillRectangle(brush, x * 10, y * 10, 10, 10);
+                    }
+                    Pen pen = new Pen(Color.Black);
+                    grp.DrawRectangle(pen, x * 10, y * 10, 10, 10);
+                }
+            }
+
+            
+        }
+
+        private void LoadMap()
+        {
+            MapData[0, 0].Bloq = 1;
+            MapData[1, 0].Bloq = 1;
+            MapData[2, 0].Bloq = 1;
+            MapData[3, 0].Bloq = 1;
+            MapData[4, 0].Bloq = 1;
+            MapData[5, 0].Bloq = 1;
+            MapData[6, 0].Bloq = 1;
+            MapData[7, 0].Bloq = 1;
+            MapData[8, 0].Bloq = 1;
+            MapData[9, 0].Bloq = 1;
+            MapData[10, 0].Bloq = 1;
+            MapData[11, 0].Bloq = 1;
+            MapData[12, 0].Bloq = 1;
+            MapData[13, 0].Bloq = 1;
+            MapData[14, 0].Bloq = 1;
+            MapData[15, 0].Bloq = 1;
+            MapData[16, 0].Bloq = 1;
+            MapData[17, 0].Bloq = 1;
+            MapData[18, 0].Bloq = 1;
+            MapData[19, 0].Bloq = 1;
+            MapData[20, 0].Bloq = 1;
+            MapData[21, 0].Bloq = 1;
+            MapData[22, 0].Bloq = 1;
+            MapData[23, 0].Bloq = 1;
+            MapData[24, 0].Bloq = 1;
+            MapData[25, 0].Bloq = 1;
+            MapData[26, 0].Bloq = 1;
+            MapData[27, 0].Bloq = 1;
+            MapData[28, 0].Bloq = 1;
+            MapData[29, 0].Bloq = 1;
+            MapData[30, 0].Bloq = 1;
+            MapData[31, 0].Bloq = 1;
+            MapData[32, 0].Bloq = 1;
+            MapData[33, 0].Bloq = 1;
+            MapData[34, 0].Bloq = 1;
+            MapData[35, 0].Bloq = 1;
+            MapData[36, 0].Bloq = 1;
+            MapData[37, 0].Bloq = 1;
+            MapData[38, 0].Bloq = 1;
+            MapData[39, 0].Bloq = 1;
+            MapData[0, 29].Bloq = 1;
+            MapData[1, 29].Bloq = 1;
+            MapData[2, 29].Bloq = 1;
+            MapData[3, 29].Bloq = 1;
+            MapData[4, 29].Bloq = 1;
+            MapData[5, 29].Bloq = 1;
+            MapData[6, 29].Bloq = 1;
+            MapData[7, 29].Bloq = 1;
+            MapData[8, 29].Bloq = 1;
+            MapData[9, 29].Bloq = 1;
+            MapData[10, 29].Bloq = 1;
+            MapData[11, 29].Bloq = 1;
+            MapData[12, 29].Bloq = 1;
+            MapData[13, 29].Bloq = 1;
+            MapData[14, 29].Bloq = 1;
+            MapData[15, 29].Bloq = 1;
+            MapData[16, 29].Bloq = 1;
+            MapData[17, 29].Bloq = 1;
+            MapData[18, 29].Bloq = 1;
+            MapData[19, 29].Bloq = 1;
+            MapData[20, 29].Bloq = 1;
+            MapData[21, 29].Bloq = 1;
+            MapData[22, 29].Bloq = 1;
+            MapData[23, 29].Bloq = 1;
+            MapData[24, 29].Bloq = 1;
+            MapData[25, 29].Bloq = 1;
+            MapData[26, 29].Bloq = 1;
+            MapData[27, 29].Bloq = 1;
+            MapData[28, 29].Bloq = 1;
+            MapData[29, 29].Bloq = 1;
+            MapData[30, 29].Bloq = 1;
+            MapData[31, 29].Bloq = 1;
+            MapData[32, 29].Bloq = 1;
+            MapData[33, 29].Bloq = 1;
+            MapData[34, 29].Bloq = 1;
+            MapData[35, 29].Bloq = 1;
+            MapData[36, 29].Bloq = 1;
+            MapData[37, 29].Bloq = 1;
+            MapData[38, 29].Bloq = 1;
+            MapData[39, 29].Bloq = 1;
+            MapData[0, 1].Bloq = 1;
+            MapData[0, 2].Bloq = 1;
+            MapData[0, 3].Bloq = 1;
+            MapData[0, 4].Bloq = 1;
+            MapData[0, 5].Bloq = 1;
+            MapData[0, 6].Bloq = 1;
+            MapData[0, 7].Bloq = 1;
+            MapData[0, 8].Bloq = 1;
+            MapData[0, 9].Bloq = 1;
+            MapData[0, 10].Bloq = 1;
+            MapData[0, 11].Bloq = 1;
+            MapData[0, 12].Bloq = 1;
+            MapData[0, 13].Bloq = 1;
+            MapData[0, 14].Bloq = 1;
+            MapData[0, 15].Bloq = 1;
+            MapData[0, 16].Bloq = 1;
+            MapData[0, 17].Bloq = 1;
+            MapData[0, 18].Bloq = 1;
+            MapData[0, 19].Bloq = 1;
+            MapData[0, 20].Bloq = 1;
+            MapData[0, 21].Bloq = 1;
+            MapData[0, 22].Bloq = 1;
+            MapData[0, 23].Bloq = 1;
+            MapData[0, 24].Bloq = 1;
+            MapData[0, 25].Bloq = 1;
+            MapData[0, 26].Bloq = 1;
+            MapData[0, 27].Bloq = 1;
+            MapData[0, 28].Bloq = 1;
+            MapData[0, 29].Bloq = 1;
+            MapData[39, 1].Bloq = 1;
+            MapData[39, 2].Bloq = 1;
+            MapData[39, 3].Bloq = 1;
+            MapData[39, 4].Bloq = 1;
+            MapData[39, 5].Bloq = 1;
+            MapData[39, 6].Bloq = 1;
+            MapData[39, 7].Bloq = 1;
+            MapData[39, 8].Bloq = 1;
+            MapData[39, 9].Bloq = 1;
+            MapData[39, 10].Bloq = 1;
+            MapData[39, 11].Bloq = 1;
+            MapData[39, 12].Bloq = 1;
+            MapData[39, 13].Bloq = 1;
+            MapData[39, 14].Bloq = 1;
+            MapData[39, 15].Bloq = 1;
+            MapData[39, 16].Bloq = 1;
+            MapData[39, 17].Bloq = 1;
+            MapData[39, 18].Bloq = 1;
+            MapData[39, 19].Bloq = 1;
+            MapData[39, 20].Bloq = 1;
+            MapData[39, 21].Bloq = 1;
+            MapData[39, 22].Bloq = 1;
+            MapData[39, 23].Bloq = 1;
+            MapData[39, 24].Bloq = 1;
+            MapData[39, 25].Bloq = 1;
+            MapData[39, 26].Bloq = 1;
+            MapData[39, 27].Bloq = 1;
+            MapData[39, 28].Bloq = 1;
+            MapData[39, 29].Bloq = 1;
+
+            MapData[StartPos.X, StartPos.Y].Type = 1;
+            MapData[EndPos.X, EndPos.Y].Type = 2;
+        }
+
+        private void cmdSimplePath_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            int firsti = 0;
+            int lasti = 0;
+            int newfirsti = 0;
+            int newlasti = 0;
+            int lastpath = 0;
+            int iterations = 0;
+            bool finished = false;
+            bool used = false;
+            bool unreachable = false;
+            Stopwatch watch = new Stopwatch();
+
+            List<PathInfo> Path = new List<PathInfo>();
+            Path.Add(new PathInfo(StartPos.X, StartPos.Y, i));
+
+            ClearPaths();
+
+            watch.Start();
+            do
+            {
+                iterations++;
+                newfirsti = 0;
+                for (i = firsti; i <= lasti; i++)
+                {
+                    PathInfo tmpPath = Path[i];
+                    if (MapData[tmpPath.X, tmpPath.Y - 1].Bloq == 0)
+                    {
+                        used = false;
+                        if (MapData[tmpPath.X, tmpPath.Y - 1].tmpPath == 1) { used = true; }
+
+                        if (used == false)
+                        {
+                            newlasti++;
+                            if (newfirsti == 0) { newfirsti = newlasti; }
+                            Path.Add(new PathInfo(tmpPath.X, tmpPath.Y - 1, i));
+                            MapData[tmpPath.X, tmpPath.Y - 1].tmpPath = 1;
+                            if (MapData[tmpPath.X, tmpPath.Y - 1].Type == 2)
+                            {
+                                finished = true;
+                                lastpath = i;
+                                break;
+                            }
+                        }
+                    }
+                    if (MapData[tmpPath.X, tmpPath.Y + 1].Bloq == 0)
+                    {
+                        used = false;
+                        if (MapData[tmpPath.X, tmpPath.Y + 1].tmpPath == 1) { used = true; }
+
+                        if (used == false)
+                        {
+                            newlasti++;
+                            if (newfirsti == 0) { newfirsti = newlasti; }
+                            Path.Add(new PathInfo(tmpPath.X, tmpPath.Y + 1, i));
+                            MapData[tmpPath.X, tmpPath.Y + 1].tmpPath = 1;
+                            if (MapData[tmpPath.X, tmpPath.Y + 1].Type == 2)
+                            {
+                                finished = true;
+                                lastpath = i;
+                                break;
+                            }
+                        }
+                    }
+                    if (MapData[tmpPath.X + 1, tmpPath.Y].Bloq == 0)
+                    {
+                        used = false;
+                        if (MapData[tmpPath.X + 1, tmpPath.Y].tmpPath == 1) { used = true; }
+
+                        if (used == false)
+                        {
+                            newlasti++;
+                            if (newfirsti == 0) { newfirsti = newlasti; }
+                            Path.Add(new PathInfo(tmpPath.X + 1, tmpPath.Y, i));
+                            MapData[tmpPath.X + 1, tmpPath.Y].tmpPath = 1;
+                            if (MapData[tmpPath.X + 1, tmpPath.Y].Type == 2)
+                            {
+                                finished = true;
+                                lastpath = i;
+                                break;
+                            }
+                        }
+                    }
+                    if (MapData[tmpPath.X - 1, tmpPath.Y].Bloq == 0)
+                    {
+                        used = false;
+                        if (MapData[tmpPath.X - 1, tmpPath.Y].tmpPath == 1) { used = true; }
+
+                        if (used == false)
+                        {
+                            newlasti++;
+                            if (newfirsti == 0) { newfirsti = newlasti; }
+                            Path.Add(new PathInfo(tmpPath.X - 1, tmpPath.Y, i));
+                            MapData[tmpPath.X - 1, tmpPath.Y].tmpPath = 1;
+                            if (MapData[tmpPath.X - 1, tmpPath.Y].Type == 2)
+                            {
+                                finished = true;
+                                lastpath = i;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (lasti == newlasti)
+                {
+                    MessageBox.Show("Target unreachable.");
+                    unreachable = true;
+                    break; 
+                }
+                lasti = newlasti;
+                firsti = newfirsti;
+                if (Options.ShowTempPaths) { Pantalla.Refresh(); }
+            }
+            while (finished == false);
+
+            watch.Stop();
+            txtIteraciones.Text = iterations.ToString();
+            txtTiempo.Text = String.Format("{0} ms",watch.Elapsed.Milliseconds.ToString());
+
+            ClearPaths();
+            if (unreachable == false)
+            {
+                do
+                {
+                    PathInfo lastPath = Path[lastpath];
+                    MapData[lastPath.X, lastPath.Y].Path = 1;
+                    lastpath = lastPath.PREV;
+                    Pantalla.Refresh();
+                }
+                while (lastpath > 0);
+            }
+        }
+
+        private void ClearPaths()
+        {
+            int x = 0;
+            int y = 0;
+
+            for (x = 1; x < 39; x++)
+            {
+                for (y = 1; y < 29; y++)
+                {
+                    MapData[x, y].Path = 0;
+                    MapData[x, y].tmpPath = 0;
+                }
+            }
+            Pantalla.Refresh();
+        }
+
+        private void Pantalla_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var pos = Pantalla.PointToClient(Cursor.Position);
+                int posx = pos.X / 10;
+                int posy = pos.Y / 10;
+                if ((posx < 1) || (posx > 38) || (posy < 1) || (posy > 28)) { return; }
+                if ((LastMouseX != posx) || (LastMouseY != posy))
+                {
+                    LastMouseX = posx;
+                    LastMouseY = posy;
+                    if (MapData[posx, posy].Bloq == 0)
+                    {
+                        MapData[posx, posy].Bloq = 1;
+                    }
+                    else
+                    {
+                        MapData[posx, posy].Bloq = 0;
+                    }
+                    Pantalla.Refresh();
+                }
+            }
+        }
+
+        private void cmdClear_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+
+            for (x = 1; x < 39; x++)
+            {
+                for (y = 1; y < 29; y++)
+                {
+                    MapData[x, y].Path = 0;
+                    MapData[x, y].Bloq = 0;
+                }
+            }
+
+            Pantalla.Refresh();
+        }
+
+        private void cmdClearPath_Click(object sender, EventArgs e)
+        {
+            ClearPaths();
+        }
+
+        private void Pantalla_Click(object sender, EventArgs e)
+        {
+            var pos = Pantalla.PointToClient(Cursor.Position);
+            int posx = pos.X / 10;
+            int posy = pos.Y / 10;
+            if ((posx < 1) || (posx > 38) || (posy < 1) || (posy > 28)) { return; }
+            if ((LastMouseX != posx) || (LastMouseY != posy))
+            {
+                LastMouseX = posx;
+                LastMouseY = posy;
+                if (MapData[posx, posy].Bloq == 0)
+                {
+                    MapData[posx, posy].Bloq = 1;
+                }
+                else
+                {
+                    MapData[posx, posy].Bloq = 0;
+                }
+                Pantalla.Refresh();
+            }
+        }
+
+        private void mnuDTempPath_Click(object sender, EventArgs e)
+        {
+            if (mnuDTempPath.Checked == true)
+            {
+                mnuDTempPath.Checked = false;
+                Options.ShowTempPaths = false;
+            }
+            else
+            {
+                mnuDTempPath.Checked = true;
+                Options.ShowTempPaths = true;
+            }
+        }
+    }
+
+    public class PathInfo
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int PREV { get; set; }
+
+        public PathInfo(int x, int y, int prev)
+        {
+            X = x;
+            Y = y;
+            PREV = prev;
+        }
+    }
+
+    public struct sMapData
+    {
+        public byte Type;
+        public byte Bloq;
+        public byte Path;
+        public byte tmpPath;
+    }
+
+    public struct sPos
+    {
+        public byte X;
+        public byte Y;
+
+        public sPos(byte x, byte y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public struct sOptions
+    {
+        public bool ShowTempPaths;
+    }
+
+}
